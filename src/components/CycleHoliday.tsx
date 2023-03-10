e40db1ec6e04f19174aaa34c98779dc5dd57f59b
Justin <janthony@udel.edu>
2023-03-09 23:36:05 -0500

    finished CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🎄" | "👻" | "🐇" | "🐲" | "💕";
+
+    const [holiday, setHoliday] = useState<Holiday>("🎄");
+
+    const YEAR_TRANSITIONS: Record<Holiday, Holiday> = {
+        "🐲": "💕",
+        "💕": "🐇",
+        "🐇": "👻",
+        "👻": "🎄",
+        "🎄": "🐲"
+    };
+
+    const ALPHA_TRANSITIONS: Record<Holiday, Holiday> = {
+        "🐲": "🎄",
+        "🎄": "🐇",
+        "🐇": "👻",
+        "👻": "💕",
+        "💕": "🐲"
+    };
+    function changeAlpha(): void {
+        const newHoliday = ALPHA_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+    function changeYear(): void {
+        const newHoliday = YEAR_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            <span>
+                <Button onClick={changeAlpha}>Advance by Alphabet</Button>
+            </span>
+            <span>
+                <Button onClick={changeYear}>Advance by Year</Button>
+            </span>
+            <div>Holiday: {holiday}</div>
+        </div>
+    );
 }
