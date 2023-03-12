d0aa2ed0e2fd28ea8a826fad0fbb4fed47057a61
Justin <janthony@udel.edu>
2023-03-12 14:29:09 -0400

    DoubleHalf passing all tests

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+interface DoublerHalverProps {
+    // The type is "a function that consumes nothing and returns nothing"
+    doubleHalve: () => void;
 }
-
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Doubler({ doubleHalve }: DoublerHalverProps): JSX.Element {
+    return <Button onClick={doubleHalve}>Double</Button>;
 }
-
+function Halver({ doubleHalve }: DoublerHalverProps): JSX.Element {
+    return <Button onClick={doubleHalve}>Halve</Button>;
+}
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+    const doubler = () => setDhValue(2 * dhValue);
+    const halver = () => setDhValue(0.5 * dhValue);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler doubleHalve={doubler}></Doubler>
+            <Halver doubleHalve={halver}></Halver>
         </div>
     );
 }
