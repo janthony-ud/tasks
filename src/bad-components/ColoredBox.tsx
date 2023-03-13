43c35767761f1140b3660536b93d119cbabc47aa
Justin <janthony@udel.edu>
2023-03-12 20:05:40 -0400

    functionality for shovebox and coloredbox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,23 +4,26 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+interface ChangeColorProps {
+    nextcolor: () => void;
+}
+interface ColorPreviewProps {
+    a_color_index: number;
+}
+//() => setColorIndex((1 + colorIndex) % COLORS.length)
+
+function ChangeColor({ nextcolor }: ChangeColorProps): JSX.Element {
+    return <Button onClick={nextcolor}>Next Color</Button>;
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ a_color_index }: ColorPreviewProps): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[a_color_index],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +33,15 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+    const nextColor = () => setColorIndex((1 + colorIndex) % COLORS.length);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor nextcolor={nextColor}></ChangeColor>
+                <ColorPreview a_color_index={colorIndex}></ColorPreview>
             </div>
         </div>
     );
