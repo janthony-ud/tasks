bddedf651487d10299052638ef590231befc5f3b
Justin <janthony@udel.edu>
2023-03-14 17:59:46 -0400

    MultipleChoice functionality

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,23 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedAnswer, setSelectedAnswer] = useState<string>(options[0]);
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formMultipleChoice">
+                <h3>Multiple Choice Question</h3>
+                <Form.Select value={selectedAnswer} onChange={updateSelected}>
+                    {options.map((answer: string) => (
+                        <option key={answer} value={answer}>
+                            {answer}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selectedAnswer === expectedAnswer ? <div>✔️</div> : <div>❌</div>}
         </div>
     );
 }
