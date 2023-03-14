2a44abeff94ceb1eaadbce512d915b2aa9356b8a
Justin <janthony@udel.edu>
2023-03-14 14:25:29 -0400

    functionality for GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<string>("0");
+    const attempts = parseInt(attemptsRequested) || 0;
     return (
-        <div>
-            <h3>Give Attempts</h3>
-        </div>
+        <>
+            <div>
+                <Form.Group controlId="formGiveAttemmpts">
+                    <Form.Label>How Many Attempts Do You Need?: </Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={attemptsRequested}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setAttemptsRequested(event.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button
+                    onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                    disabled={attemptsLeft === 0}
+                >
+                    Use
+                </Button>
+                <Button
+                    onClick={() => setAttemptsLeft(attemptsLeft + attempts)}
+                >
+                    Gain
+                </Button>
+            </div>
+            <div>Attempts Left: {attemptsLeft}</div>
+        </>
     );
 }
